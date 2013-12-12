7bde2d02fd557b95c0015c1cdd235fea134c67e8
Pedro Luis Furio Raphael <pedroluis.raphael@gmail.com>
2013-12-12 21:24:31 -0200

    montly cost and weekly cost

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,12 +2,32 @@ class StaticPagesController < ApplicationController
 
   def home
     @daily = daily_wasted
+    @weekly = weekly_wasted
+    @monthly = monthly_wasted
   end
 
   private
 
     def daily_wasted
       medicoes = get_all_medicoes_day
+      total_wasted = 0.0
+      medicoes.each do |m|
+        total_wasted += wasted_by_medicao(m)
+      end
+      return total_wasted
+    end
+
+    def weekly_wasted
+      medicoes = get_all_medicoes_week
+      total_wasted = 0.0
+      medicoes.each do |m|
+        total_wasted += wasted_by_medicao(m)
+      end
+      return total_wasted
+    end
+
+    def monthly_wasted
+      medicoes = get_all_medicoes_month
       total_wasted = 0.0
       medicoes.each do |m|
         total_wasted += wasted_by_medicao(m)
@@ -23,8 +43,17 @@ class StaticPagesController < ApplicationController
     end
 
     def get_all_medicoes_day
-      medicoes = Medicao.find(:all, :conditions => [" created_at between ? AND ?", 
-                                          Time.zone.now.beginning_of_day, Time.zone.now.end_of_day])
+      medicoes = Medicao.find(:all, :conditions => [" created_at between ? AND ?", Time.zone.now.beginning_of_day, Time.zone.now.end_of_day])
+      medicoes
+    end
+
+    def get_all_medicoes_week
+      medicoes = Medicao.find(:all, :conditions => [" created_at between ? AND ?", Time.zone.now.beginning_of_week, Time.zone.now.end_of_week])
+      medicoes
+    end
+
+    def get_all_medicoes_month
+      medicoes = Medicao.find(:all, :conditions => [" created_at between ? AND ?", Time.zone.now.beginning_of_month, Time.zone.now.end_of_month])
       medicoes
     end
 end
